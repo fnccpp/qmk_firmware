f550bc7736917ac18430ca6f1ef17c9c8d98ff01
Francesco <72805187+fnccpp@users.noreply.github.com>
2024-01-28 11:49:54 +0100

    Tastierino numerico

--- ULTIMA CONFIG/keymap.c
+++ ULTIMA CONFIG/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_split_3x6_3( //NUMERI
   //,--------------------------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,                KC_F1,   KC_F2,   KC_F3,   KC_F4,  XXXXXXX,                      KC_PAST,     KC_1,    KC_2,    KC_3,  IT_EQL, KC_BSPC,
+       KC_ESC,                KC_F1,   KC_F2,   KC_F3,   KC_F4,  XXXXXXX,                      KC_PAST,     KC_7,    KC_8,    KC_9,  IT_EQL, KC_BSPC,
   //|-----------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       MT(MOD_LSFT, KC_CAPS),        KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_PDOT,                      KC_PSLS,  KC_4,    KC_5,    KC_6,   IT_MINS, KC_ENT,
   //|-----------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,                KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_COMM,                      KC_PPLS,  KC_7,    KC_8,    KC_9,   KC_LEFT,  KC_RGHT,
+      KC_LCTL,                KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_COMM,                      KC_PPLS,  KC_1,    KC_2,    KC_3,   KC_LEFT,  KC_RGHT,
   //|-----------------------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                                         KC_LALT, _______, KC_SPC,    KC_0, MO(3), KC_LGUI
                                                      //`--------------------------'  `--------------------------'
@@ -144,6 +144,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         } else { 
             if (delkey_registered) {
+            //     unregister_code(KC_MINS);
                  delkey_registered = false;
                  return false;
             }
