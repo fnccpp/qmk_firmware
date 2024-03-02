7ae1e893fa689c442051f3d4aa66f7cef3a74bf3
Francesco <72805187+fnccpp@users.noreply.github.com>
2024-03-02 16:03:28 +0100

    Oled (status + bongocat)

---
+++ ULTIMA CONFIG/rules.mk
@@ -0,0 +1,20 @@
+# Build Options
+#   change yes to no to disable
+#
+
+# If you want to change the display of OLED, you need to change here
+# SRC += ./logo_reader.c
+
+# enable OLED displays
+OLED_ENABLE = yes
+WPM_ENABLE = yes
+LTO_ENABLE = yes
+
+DEFAULT_FOLDER = crkbd/rev1
+
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+
+MOUSEKEY_ENABLE = yes
+
+CONSOLE_ENABLE = no 
